d7c7429210bf664bdec1afce122fa22f43e2f271
iamarale <ismailarales@gmail.com>
2024-09-13 13:06:22 -0500

    feat(active): added active class depending on which section on in view

--- js/app.js
+++ js/app.js
@@ -17,32 +17,19 @@
  * Great to have comments before crucial code sections within the procedure.
  */
 
-/* 
-    Define Global Variables 
-*/
-// Dom el's
+/* Define Global Variables */
+
+// DOM el's
 const navbarLists = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
 // empty li to be filled later
 let li = [];
 
-/* 
-    End Global Variables 
-*/
+/* End Global Variables */
 
 /* Start Helper Functions */
-function makeActive() {
-  for (const section of sections) {
-    const box = section.getBoundingClientRect();
-    //Find a value that works best, but 150 seems to be a good start.
-    if (box.top <= VALUE && box.bottom >= VALUE) {
-      //apply active state on current section and corresponding Nav link
-    } else {
-      //Remove active state from other section and corresponding Nav link
-    }
-  }
-}
+
 /* 
     End Helper Functions
 */
@@ -51,13 +38,38 @@ function makeActive() {
     Begin Main Functions 
 */
 
-/* 
-    build the nav 
-*/
+/* build the nav */
 
-/* 
-    Add class 'active' to section when near top of viewport 
-*/
+// creates navbar links
+for (let i = 1; i < sections.length + 1; i++) {
+  // creates li with class "nav-li"
+  const createLi = document.createElement("li");
+  createLi.textContent = `Section ${i}`;
+  createLi.classList.add("nav__li");
+
+  // adds to navbar ul
+  navbarLists.appendChild(createLi);
+}
+
+/* Add class 'active' to section when near top of viewport */
+function makeActive() {
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    //Find a value that works best, but 150 seems to be a good start.
+    if (box.top <= 0 && box.bottom >= 200) {
+      //apply active state on current section and corresponding Nav link
+      console.log(section);
+      section.classList.add("active");
+    } else {
+      //Remove active state from other section and corresponding Nav link
+      section.classList.remove("active");
+    }
+  }
+}
+
+document.addEventListener("scroll", function () {
+  makeActive();
+});
 
 /* 
     Scroll to anchor ID using scrollTO event 
