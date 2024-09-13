6d547d4357db6bebaf58bea554ab6a2ca6697fb3
iamarale <ismailarales@gmail.com>
2024-09-13 15:54:41 -0500

    pos func created

--- js/app.js
+++ js/app.js
@@ -26,26 +26,21 @@ const sections = document.querySelectorAll("section");
 // empty li to be filled later
 let li = [];
 
-/* End Global Variables */
+/* Helper Functions */
 
-/* Start Helper Functions */
-
-/* 
-    End Helper Functions
-*/
-
-/* 
-    Begin Main Functions 
-*/
+/* Main Functions */
 
 /* build the nav */
-
 // creates navbar links
 for (let i = 1; i < sections.length + 1; i++) {
   // creates li with class "nav-li"
   const createLi = document.createElement("li");
   createLi.textContent = `Section ${i}`;
   createLi.classList.add("nav__li");
+  createLi.dataset.nav = `Section ${i}`;
+
+  // adds links to li
+  li.push(createLi);
 
   // adds to navbar ul
   navbarLists.appendChild(createLi);
@@ -58,7 +53,6 @@ function makeActive() {
     //Find a value that works best, but 150 seems to be a good start.
     if (box.top <= 0 && box.bottom >= 200) {
       //apply active state on current section and corresponding Nav link
-      console.log(section);
       section.classList.add("active");
     } else {
       //Remove active state from other section and corresponding Nav link
@@ -67,30 +61,31 @@ function makeActive() {
   }
 }
 
-document.addEventListener("scroll", function () {
-  makeActive();
+/* Scroll to anchor ID using scrollTO event */
+li.map((el, i) => {
+  el.addEventListener("click", function (e) {
+    console.log(i + 1);
+    if (
+      sections[i].dataset.nav === `Section ${i + 1}` &&
+      el.dataset.nav === `Section ${i + 1}`
+    ) {
+      sections[i].scrollTo(150, 20);
+    }
+  });
 });
 
-/* 
-    Scroll to anchor ID using scrollTO event 
-*/
+/* Begin Events */
+document.addEventListener("scroll", function () {
+  makeActive();
+  getPosition(sections[4]);
+});
+/* Build menu */
 
-/* 
-    End Main Functions 
-*/
-
-/* 
-    Begin Events 
-*/
-
-/* 
-    Build menu 
-*/
-
-/* 
-    Scroll to section on link click 
-*/
-
+/* Scroll to section on link click */
+const getPosition = function (pos) {
+  const position = pos.getBoundingClientRect();
+  console.log();
+};
 /* 
     Set sections as active
 */
